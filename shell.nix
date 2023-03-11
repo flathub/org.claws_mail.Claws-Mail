60b5076d4faef366723b8c58813270e7cd1f0e8c
Danny van Heumen <danny@dannyvanheumen.nl>
2023-03-11 23:19:55 +0100

    Add support for nix-shell environment

---
+++ shell.nix
@@ -0,0 +1,20 @@
+{ pkgs ? import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/4d2b37a84fad1091b9de401eb450aae66f1a741e.tar.gz") {} }:
+
+pkgs.mkShell {
+  buildInputs = [
+    pkgs.which
+    pkgs.htop
+    pkgs.zlib
+    pkgs.less
+    pkgs.gitMinimal
+
+    pkgs.gnumake
+    pkgs.autoconf
+    pkgs.libtool
+    pkgs.automake
+    pkgs.pkg-config
+
+    pkgs.flatpak
+    pkgs.flatpak-builder
+  ];
+}
