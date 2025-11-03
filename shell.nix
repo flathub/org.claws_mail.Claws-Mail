c707ddcbb1275fc29a46870f252e24be1677ff78
Danny van Heumen <danny@dannyvanheumen.nl>
2025-11-03 18:32:25 +0100

    next: update nix-shell and shared-modules

--- shell.nix
+++ shell.nix
@@ -1,4 +1,4 @@
-{ pkgs ? import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/4d2b37a84fad1091b9de401eb450aae66f1a741e.tar.gz") {} }:
+{ pkgs ? import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/refs/tags/25.05.tar.gz") {} }:
 
 pkgs.mkShell {
   buildInputs = [
@@ -17,5 +17,6 @@ pkgs.mkShell {
 
     pkgs.flatpak
     pkgs.flatpak-builder
+    pkgs.appstream
   ];
 }
